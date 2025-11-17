58e09736c133876bea5d0fa0320be4c5cb40eba1
James O'Hea <James.OHea@diamond.ac.uk>
2025-11-17 16:24:01 +0000

    Make TemperatureAttributeIO type generic

--- src/fastcs_jungfrau/io/temperature_attribute_io.py
+++ src/fastcs_jungfrau/io/temperature_attribute_io.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from fastcs.attribute_io import AttributeIO
 from fastcs.attribute_io_ref import AttributeIORef
 from fastcs.attributes import AttrR
+from fastcs.datatypes import T
 from slsdet import Jungfrau
 
 
@@ -12,13 +13,13 @@ class TemperatureAttributeIORef(AttributeIORef):
     temperature_index: int
 
 
-class TemperatureAttributeIO(AttributeIO[int, TemperatureAttributeIORef]):
+class TemperatureAttributeIO(AttributeIO[T, TemperatureAttributeIORef]):
     def __init__(self, detector: Jungfrau):
         self.detector = detector
 
         super().__init__()
 
-    async def update(self, attr: AttrR[int, TemperatureAttributeIORef]):
+    async def update(self, attr: AttrR[T, TemperatureAttributeIORef]):
         temperature = self.detector.getTemperature(attr.io_ref.temperature_index)[
             attr.io_ref.module_index
         ]
